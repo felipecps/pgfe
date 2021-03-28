63ccd83839db8bd72cb6855e7f935072f190a995
Felipe <Felipe@DESKTOP-EDKQLLT>
2021-03-28 15:41:53 -0300

    Atualização no exercicio 2

--- frontend/src/components/exerciciosEstruturaSequencial/ex2.vue
+++ frontend/src/components/exerciciosEstruturaSequencial/ex2.vue
@@ -2,13 +2,13 @@
     <div class="container">
         <div class="mt-3">
             <b-card bg-variant="light" text-variant="black">
-                <b-form @submit="onSubmit">
+                <b-form @submit="onSubmit" @reset="onReset">
 
-                    <TextoIntrodutorio v-show="true"
-                                       :texto="texto2">
-                    </TextoIntrodutorio>
+                    <TextoIntrodutorio v-show="true" :texto="texto2"></TextoIntrodutorio>
 
                     <b-button class="mr-1" @click="submit('2')" variant="primary">Executar</b-button>
+                    <b-button type="reset" variant="danger">Limpar</b-button>
+                    
                     <div class="mt-3" v-if="mostrar_campos_para_entrada_de_dados_ex2">
                         <b-form-group id="input-exercicio-2" label="Informe um número:" label-for="exercicio-2"
                                       description="O número será enviado para o servidor python e na sequência devolvido em uma sentença abaixo.">
@@ -20,7 +20,9 @@
                                           @input="submit(2)"></b-form-input>
                         </b-form-group>
                     </div>
-                    <b-alert class="mt-3" v-if="resposta02" show>{{ resposta02 }}</b-alert>
+                    <div v-if="mostra_resposta">
+                        <b-alert class="mt-3"  show>{{ resposta02 }}</b-alert>
+                    </div>
                 </b-form>
             </b-card>
         </div>
@@ -51,17 +53,12 @@
         },
         methods: {
             submit(nro_do_exercicio) {
-                if (nro_do_exercicio == '2') {
-                    this.post = true
-                    this.mostrar_campos_para_entrada_de_dados_ex2 = true
+                this.mostrar_campos_para_entrada_de_dados_ex2 = true
 
-                    if (this.form.nro2 == '') {
-                        this.post = false
-                    }
-                } 
                 //const path = 'http://localhost:5000/resolve_exercicios'
                 const path = 'https://felipecps.pythonanywhere.com/resolve_exercicios'
-                if (this.post) {
+
+                if (this.form.nro2 != '') {
                     axios.post(
                         path,
                         null,
@@ -82,11 +79,15 @@
                             console.error(error);
                         })
                 }
-            },
+             },
             onSubmit(evt) {
                 evt.preventDefault()
-
-            }
+            },
+            onReset(event) {
+                event.preventDefault()
+                this.form.nro2 = '',
+                this.mostra_resposta = false
+            }   
         },
         created() {
 
