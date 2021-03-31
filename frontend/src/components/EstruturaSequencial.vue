06baf11d5c1e4ead37206121e63bf166c8b9680b
Felipe <Felipe@DESKTOP-EDKQLLT>
2021-03-30 21:36:30 -0300

    minor

--- frontend/src/components/EstruturaSequencial.vue
+++ frontend/src/components/EstruturaSequencial.vue
@@ -1,22 +1,22 @@
 ﻿<template>
     <div class="container">
         <div>
-            <b-link href="/python/estruturaSequencial/ex1">Exercicio 1</b-link>
+            <b-link href="/python/estruturaSequencial/ex1">Exercício 1</b-link>
         </div>
         <div>
-            <b-link href="/python/estruturaSequencial/ex2">Exercicio 2</b-link>
+            <b-link href="/python/estruturaSequencial/ex2">Exercício 2</b-link>
         </div>
         <div>
-            <b-link href="/python/estruturaSequencial/ex3">Exercicio 3</b-link>
+            <b-link href="/python/estruturaSequencial/ex3">Exercício 3</b-link>
         </div>
         <div>
-            <b-link href="/python/estruturaSequencial/ex4">Exercicio 4</b-link>
+            <b-link href="/python/estruturaSequencial/ex4">Exercício 4</b-link>
         </div>
         <div>
-            <b-link href="/python/estruturaSequencial/ex5">Exercicio 5</b-link>
+            <b-link href="/python/estruturaSequencial/ex5">Exercício 5</b-link>
         </div>
         <div>
-            <b-link href="/python/estruturaSequencial/ex6">Exercicio 6</b-link>
+            <b-link href="/python/estruturaSequencial/ex6">Exercício 6</b-link>
         </div>
     </div>
 </template>
--- frontend/src/components/exerciciosEstruturaSequencial/ex6.vue
+++ frontend/src/components/exerciciosEstruturaSequencial/ex6.vue
@@ -7,7 +7,7 @@
                 <!-- <b-button class="mr-1" @click="submit()" variant="primary">Executar</b-button> -->
                 <div class="mt-3" v-if="mostrar_campos_para_entrada_de_dados_ex6">
                     <b-form @submit="onSubmit" @reset="onReset">
-                        <b-form-group id="input-exercicio-6" label="Digite o raio do circulo em metros:" label-for="input-1">
+                        <b-form-group id="input-exercicio-6" label="Digite o raio do círculo em metros:" label-for="input-1">
                             <b-form-input id="input-1"
                                           autocomplete="off"
                                           v-model="form.nro1"
@@ -15,7 +15,7 @@
                                           required></b-form-input>
                         </b-form-group>
 
-                        <b-button class="mr-1" type="submit" variant="primary">Calcular área do circulo</b-button>
+                        <b-button class="mr-1" type="submit" variant="primary">Calcular área do círculo</b-button>
                         <b-button type="reset" variant="danger" :disabled="(mostra_resposta == false && form.nro1 == '')">Limpar</b-button>
                     </b-form>
                     <b-alert class="mt-3" v-if="mostra_resposta" show>{{ intro_resposta }} {{ resposta06 }} {{ m2 }}</b-alert>
@@ -56,8 +56,8 @@
             onSubmit(event) {
                 event.preventDefault()
 
-                const path = 'http://localhost:5000/resolve_exercicios'
-                //const path = 'https://felipecps.pythonanywhere.com/resolve_exercicios'
+                //const path = 'http://localhost:5000/resolve_exercicios'
+                const path = 'https://felipecps.pythonanywhere.com/resolve_exercicios'
 
                 if (this.form.nro1 != '') {
                     axios.post(
