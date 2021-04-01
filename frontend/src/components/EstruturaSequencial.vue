4578286059b9b994c4af8ee4fa94d926e2b79e8b
Felipe <Felipe@DESKTOP-EDKQLLT>
2021-03-31 21:35:45 -0300

    Mudando como os links são formados

--- frontend/src/components/EstruturaSequencial.vue
+++ frontend/src/components/EstruturaSequencial.vue
@@ -1,22 +1,43 @@
 ﻿<template>
     <div class="container">
-        <div>
-            <b-link href="/python/estruturaSequencial/ex1">Exercício 1</b-link>
-        </div>
-        <div>
-            <b-link href="/python/estruturaSequencial/ex2">Exercício 2</b-link>
-        </div>
-        <div>
-            <b-link href="/python/estruturaSequencial/ex3">Exercício 3</b-link>
-        </div>
-        <div>
-            <b-link href="/python/estruturaSequencial/ex4">Exercício 4</b-link>
-        </div>
-        <div>
-            <b-link href="/python/estruturaSequencial/ex5">Exercício 5</b-link>
-        </div>
-        <div>
-            <b-link href="/python/estruturaSequencial/ex6">Exercício 6</b-link>
+        <div v-for="link in links">            
+            <b-link :href="link.nro">{{ link.texto }}</b-link>
         </div>
     </div>
 </template>
+
+
+<script>
+        //import TextoIntrodutorio from "@/components/TextoIntrodutorio.vue";
+
+        export default {
+            components: {
+                //TextoIntrodutorio
+            },
+            data() {
+                return {
+                    links: [
+                        { nro: "/python/estruturaSequencial/ex1", texto: "1. Faça um Programa que mostre a mensagem 'Alo mundo' na tela." }, 
+                        { nro: "/python/estruturaSequencial/ex2", texto: "2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]." },
+                        { nro: "/python/estruturaSequencial/ex3", texto: "3. Faça um Programa que peça dois números e imprima a soma." },
+                        { nro: "/python/estruturaSequencial/ex4", texto: "4. Faça um Programa que peça as 4 notas bimestrais e mostre a média." },
+                        { nro: "/python/estruturaSequencial/ex5", texto: "5. Faça um Programa que converta metros para centímetros." },
+                        { nro: "/python/estruturaSequencial/ex6", texto: "6. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área." }
+                    ]
+                };
+            },
+            methods: {
+                
+                onSubmit(evt) {
+                    evt.preventDefault()
+                },
+                onReset(event) {
+                    event.preventDefault()
+                    
+                }
+            },
+            created() {
+
+            },
+        };
+</script>
