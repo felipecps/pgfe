3857d6cf073b31e98c19a11d6d8f7e0a87bfafce
Felipe <Felipe@DESKTOP-EDKQLLT>
2021-04-17 16:24:04 -0300

    Path

--- frontend/src/components/exerciciosEstruturaSequencial/utils/valores.js
+++ frontend/src/components/exerciciosEstruturaSequencial/utils/valores.js
@@ -1,6 +1,6 @@
 ﻿module.exports = {
-    path: "http://localhost:5000/resolve_exercicios",
-    path2: "https://felipecps.pythonanywhere.com/resolve_exercicios",
+    path2: "http://localhost:5000/resolve_exercicios",
+    path: "https://felipecps.pythonanywhere.com/resolve_exercicios",
     home_text: "Home",
     home_path: "/python",
     estruturaSequencial_text: "Estrutura Sequencial",
