f59989198f72b7ef94a7fa6da688ef6201f9833e
Felipe <Felipe@DESKTOP-EDKQLLT>
2021-04-21 21:16:03 -0300

    Adicionando vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,14 @@ import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import vuetify from './plugins/vuetify';
 
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
 new Vue({
-    router,
-    render: h => h(App),
-  }).$mount('#app');
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app');
