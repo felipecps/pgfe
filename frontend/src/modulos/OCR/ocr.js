d19908419caa855f95f46fe2fb9903984ceee93d
Felipe <Felipe@DESKTOP-EDKQLLT>
2021-05-01 13:54:49 -0300

    Adicionando um começo de OCR. O código atual funciona com o WebServer for Chrome ligado localmente.

---
+++ frontend/src/modulos/OCR/ocr.js
@@ -0,0 +1,13 @@
+﻿/*
+import Tesseract from 'tesseract.js';
+
+export function ocr() {
+    Tesseract.recognize(
+        'paint.jpg',
+        'pt',
+        { logger: m => console.log(m) }
+    ).then(({ data: { text } }) => {
+        console.log(text);
+    })
+
+}*/
