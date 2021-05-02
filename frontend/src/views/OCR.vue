6a5381aeb05a26567b37b7b7237fd0c97d570ccd
Felipe <Felipe@DESKTOP-EDKQLLT>
2021-05-01 21:10:24 -0300

    Revert "OCR funcionando para arquivos de texto"
    This reverts commit cf0dc38d59dd4a1de67b450358989994feac70f1.

--- frontend/src/views/OCR.vue
+++ frontend/src/views/OCR.vue
@@ -6,7 +6,7 @@
                           show-size
                           counter
                           label="Selecione uma imagem para extração de texto"
-                          v-model="files"
+                          v-model="imageData"
                           prepend-icon="mdi-camera"></v-file-input>
             <v-btn @click="ocr">Extrair texto</v-btn>
         </div>
@@ -28,6 +28,7 @@
     export default {
         data() {
             return {
+                imageData: [],
                 files: [],
                 resp: {
                     ocr: "",
@@ -42,43 +43,18 @@
             onUpload() {
                 mylib.ocr()              
             },
-            /*submitFiles() {
-            -                if (this.files) {
-            -                    let formData = new FormData();
-            -
-            -                    // files
-            -                    for (let file of this.files) {
-            -                        formData.append("files", file, file.name);
-            -                    }
-            -
-            -                    // additional data
-            -                    formData.append("test", "foo bar");
-            -
-            -                    axios
-            -                        .post("http://localhost:5000/ocr", formData)
-            -                        .then(response => {
-            -                            this.resp.ocr = res.data.resposta
-            -                            console.log("Success!");
-            -                            //console.log({ response });
-            -                        })
-            -                        .catch(error => {
-            -                            console.log({ error });
-            -                        });
-            -                } else {
-            -                    console.log("there are no files.");
-            -                }
-            -            },*/
             ocr() {
-                console.log(this.files)
                 Tesseract.recognize(
-                    this.files,
+                    'http://127.0.0.1:8887/eng_bw.png',
                     'eng',
                     { logger: m => console.log(m) }
                 ).then(({ data: { text } }) => {
+                    //console.log(text);
                     this.mostra_ocr = true
                     this.texto_ocr = text
                 })
             }
         }
+            
     };
 </script>
