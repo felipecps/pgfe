4d60f28378a67f3780316862854f45e12f4438aa
Felipe <Felipe@DESKTOP-EDKQLLT>
2021-05-01 21:11:05 -0300

    Revert "Adicionando vuetify"
    This reverts commit f59989198f72b7ef94a7fa6da688ef6201f9833e.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,14 +3,12 @@ import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import vuetify from './plugins/vuetify';
 
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app');
+    router,
+    render: h => h(App),
+  }).$mount('#app');
--- frontend/src/views/OCR.vue
+++ frontend/src/views/OCR.vue
@@ -1,7 +1,42 @@
 ﻿<template>
     <div class="container">
-        <v-file-input accept="image/*"
-                      label="File input"></v-file-input>
+        <div class="mb-3">
+            <p>
+                <h4>
+                    Página para extração de texto de arquivos de imagem.
+                </h4>
+            </p>
+        </div>
+        <!-- Styled -->
+        <b-form-file v-model="file1"
+                     :state="Boolean(file1)"
+                     placeholder="Escolha um arquivo ou arraste aqui..."
+                     drop-placeholder="Arraste aqui..."></b-form-file>
+        <div class="mt-3">Arquivo selecionado: {{ file1 ? file1.name : '' }}</div>
+
+        <!-- Plain mode -->
+        <b-form-file v-model="file2" class="mt-3" plain></b-form-file>
+        <div class="mt-3">Arquivo selecionado: {{ file2 ? file2.name : '' }}</div>
+
+        <div class="mt-3">
+            <p>
+                <h4>
+                    {{ texto }}
+                </h4>
+            </p>
+        </div>
+
     </div>
-    
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+          texto: "Nenhum texto extraido ainda",
+        file1: null,
+        file2: null
+      }
+    }
+  }
+</script>
